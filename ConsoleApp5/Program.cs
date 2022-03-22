aeb23cffc734777b738af59a309307aba456bf4b
MARIA03P <90255821+MARIA03P@users.noreply.github.com>
2022-03-22 20:52:21 +0300

    edit sln file

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            string Item = Console.ReadLine();
+            Console.OutputEncoding = System.Text.Encoding.ASCII;
+            Console.WriteLine(Item);
+            
+        }
+    }
+}
